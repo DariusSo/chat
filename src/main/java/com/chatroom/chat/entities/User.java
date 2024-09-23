399cd7f4a34337fbe120d7e73c6154fbc8938c18
Darius <darius.songaila1@gmail.com>
2024-09-23 20:44:01 +0300

    DAR-50

---
+++ src/main/java/com/chatroom/chat/entities/User.java
@@ -0,0 +1,29 @@
+package com.chatroom.chat.entities;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import java.util.Collection;
+
+@Entity
+@NoArgsConstructor
+@AllArgsConstructor
+@Getter
+@Setter
+@Table(name="users")
+public class User {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name="id")
+    private int id;
+    @Column(name="username")
+    private String username;
+    @Column(name="password")
+    private String password;
+    @Column(name="roles")
+    private String roles;
+}
