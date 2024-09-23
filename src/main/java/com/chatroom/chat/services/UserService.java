fb76140eb2ff7022731720f8bcd9ebe6a3c04c22
Darius <darius.songaila1@gmail.com>
2024-09-23 20:52:29 +0300

    DAR-19/20/21

---
+++ src/main/java/com/chatroom/chat/services/UserService.java
@@ -0,0 +1,17 @@
+package com.chatroom.chat.services;
+
+import com.chatroom.chat.repositories.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.stereotype.Service;
+
+@Service
+public class UserService {
+
+    private UserRepository userRepository;
+
+    @Autowired
+    public UserService(UserRepository userRepository) {
+        this.userRepository = userRepository;
+    }
+}
