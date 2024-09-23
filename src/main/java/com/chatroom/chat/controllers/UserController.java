263cdc203f54cdd7e5b00867d3115c73a04e0451
Darius <darius.songaila1@gmail.com>
2024-09-23 21:05:40 +0300

    DAR-22

--- src/main/java/com/chatroom/chat/controllers/UserController.java
+++ src/main/java/com/chatroom/chat/controllers/UserController.java
@@ -1,7 +1,10 @@
 package com.chatroom.chat.controllers;
 
+import com.chatroom.chat.entities.User;
 import com.chatroom.chat.services.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -12,5 +15,9 @@ public class UserController {
     @Autowired
     UserService userService;
 
+    @PostMapping
+    public void registerNewUser(@RequestBody User user){
+        userService.registerNewUser(user);
+    }
 
 }
